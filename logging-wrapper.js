2baa7f63368636d2700f44f8ccdf23ffebad1585
MattTurnip <mattadavey@gmail.com>
2019-03-08 00:36:20 +0000

    did some stretch work

--- logging-wrapper.js
+++ logging-wrapper.js
@@ -1,5 +1,11 @@
 var wrapLog = function (callback, name) {
-    /* your code here */
+    return function (x, y, z) {
+      if (name === "area"){
+      console.log(name + " (" +  x + "," + y + ") " + "=> " + callback(x, y, z));
+      } else if (name === "volume"){
+        console.log(name + " (" +  x + ", " + y + ", " + z + ") " + "=> " + callback(x, y, z));
+        }
+    };
   };
   
   var area = function (x, y) {
@@ -7,13 +13,15 @@ var wrapLog = function (callback, name) {
   };
   var logArea = wrapLog(area, "area");
   
-  logArea(5, 3); // area(5, 3) => 15
-  logArea(3, 2); // area(3, 2) => 6
+  logArea(5, 3); // expected output area(5, 3) => 15
+  logArea(3, 2); // expected output area(3, 2) => 6
+  logArea(100, 23);
   
   var volume = function (x, y, z) {
     return x * y * z;
   };
   var logVolume = wrapLog(volume, "volume");
   
-  logVolume(5, 3, 2); // volume(5, 3, 2) => 30
-  logVolume(3, 2, 4); // volume(3, 2, 4) => 24
+  logVolume(5, 3, 2); // expected output volume(5, 3, 2) => 30
+  logVolume(3, 2, 4); // expected output volume(3, 2, 4) => 24
+  logVolume(234, 1, 45);
